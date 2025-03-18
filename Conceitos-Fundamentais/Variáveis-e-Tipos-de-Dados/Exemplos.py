b72f4d6c3e6673b1acd25acf1121f19dfe5f3026
Nv777-Dev <nathannaelvictor1206@gmail.com>
2025-03-18 14:18:21 -0300

    Update Exemplos.py

--- Conceitos-Fundamentais/Variáveis-e-Tipos-de-Dados/Exemplos.py
+++ Conceitos-Fundamentais/Variáveis-e-Tipos-de-Dados/Exemplos.py
@@ -1,1 +1,67 @@
- 
+##Tipos Numéricos
+
+#Inteiros(int)
+
+idade = 30
+print(idade)  # Saída: 30
+
+#Flutuação(float)
+
+altura = 1.75
+print(altura)  # Saída: 1.75
+
+#Complexos(complex)
+
+numero_complexo = 3 + 4j
+print(numero_complexo)  # Saída: (3+4j)
+
+##Tipos de Dados Sequenciais
+
+#Strings(str)
+
+nome = "João"
+print(nome)  # Saída: João
+
+#Listas(list)
+
+frutas = ["maçã", "banana", "laranja"]
+print(frutas)  # Saída: ['maçã', 'banana', 'laranja']
+
+#Tuplas(tuple)
+
+cores = ("vermelho", "azul", "amarelo")
+print(cores)  # Saída: ('vermelho', 'azul', 'amarelo')
+
+##Mapeamentos
+
+#Dicionários(dict)
+
+pessoa = {"nome": "Maria", "idade": 25}
+print(pessoa["nome"])  # Saída: Maria
+
+##Conjuntos
+
+#Sets(set)
+
+numeros = {1, 2, 3}
+print(numeros)  # Saída: {1, 2, 3}
+
+#Frozensets(frozenset)
+
+frozen_numeros = frozenset([1, 2, 3])
+print(frozen_numeros)  # Saída: frozenset({1, 2, 3})
+
+##Booleano
+
+#Boolean(bool)
+
+ativo = True
+print(ativo)  # Saída: True
+
+##NoneType
+
+#None
+
+sem_valor = None
+print(sem_valor)  # Saída: None
+
