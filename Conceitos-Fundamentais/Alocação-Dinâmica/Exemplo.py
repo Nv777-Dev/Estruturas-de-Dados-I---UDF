71e08d9cb7101991ba763a0452b3565945019596
Nv777-Dev <nathannaelvictor1206@gmail.com>
2025-03-18 14:48:45 -0300

    Update Exemplo.py

--- Conceitos-Fundamentais/Alocação-Dinâmica/Exemplo.py
+++ Conceitos-Fundamentais/Alocação-Dinâmica/Exemplo.py
@@ -1,1 +1,14 @@
- 
+## Alocação dinâmica com uma lista que cresce conforme necessário
+
+dynamic_memory = []  # Inicialmente vazia
+
+dynamic_memory.append(1)
+dynamic_memory.append(2)
+dynamic_memory.append(3)
+
+print(dynamic_memory)  # Saída: [1, 2, 3]
+
+# Removendo elementos dinamicamente
+dynamic_memory.pop()
+print(dynamic_memory)  # Saída: [1, 2]
+
