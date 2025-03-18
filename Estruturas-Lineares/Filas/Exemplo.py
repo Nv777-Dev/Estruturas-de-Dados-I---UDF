520754d35de38ec1ec2dcdb55be2f00ab99d2f5a
Nv777-Dev <nathannaelvictor1206@gmail.com>
2025-03-18 15:35:36 -0300

    Update Exemplo.py

--- Estruturas-Lineares/Filas/Exemplo.py
+++ Estruturas-Lineares/Filas/Exemplo.py
@@ -1,1 +1,19 @@
+from collections import deque
+
+##Filas
+
+# Criando uma fila
+
+fila = deque()
+
+fila.append(1)  # Adiciona no final
+fila.append(2)
+fila.append(3)
+
+# Remoção
+print(fila.popleft())  # Remove e retorna o primeiro elemento              #Saída: 1
+
+# Verificação de Vazio
+print(len(fila) == 0)  # Verifica se a fila está vazia                     #Saída: False
+                                                                                      
 
