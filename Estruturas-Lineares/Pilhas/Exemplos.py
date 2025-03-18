61910dd035e9f11863c6face7f89a8399b81aa6f
Nv777-Dev <nathannaelvictor1206@gmail.com>
2025-03-18 15:41:18 -0300

    Update Exemplos.py

--- Estruturas-Lineares/Pilhas/Exemplos.py
+++ Estruturas-Lineares/Pilhas/Exemplos.py
@@ -1,1 +1,12 @@
- 
+##Pilhas
+
+# Criando uma pilha
+pilha = []
+
+pilha.append(1)  # Adiciona no topo
+pilha.append(2)
+pilha.append(3)
+
+print(pilha.pop())  # Remove e retorna o último elemento          #Saída: 3
+
+print(len(pilha) == 0)  # Verifica se a pilha está vazia          #Saída: False
