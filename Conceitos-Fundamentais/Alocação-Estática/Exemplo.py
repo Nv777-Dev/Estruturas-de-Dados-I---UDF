24c74f2a545fd6b4434202d4d6aed07bd8a4d093
Nv777-Dev <nathannaelvictor1206@gmail.com>
2025-03-18 14:31:14 -0300

    Update Exemplo.py

--- Conceitos-Fundamentais/Alocação-Estática/Exemplo.py
+++ Conceitos-Fundamentais/Alocação-Estática/Exemplo.py
@@ -1,1 +1,9 @@
- 
+## Simulação de alocação estática com lista de tamanho fixo
+
+vetor = [0] * 10  # Aloca espaço para 10 elementos com valor inicial 0
+
+vetor[0] = 1
+vetor[1] = 2
+vetor[2] = 3
+
+print(vetor)  # Saída: [1, 2, 3, 0, 0, 0, 0, 0, 0, 0]
