d8d3c5391889dc81ef15a2f29d8be6163453a265
Nv777-Dev <nathannaelvictor1206@gmail.com>
2025-03-18 15:32:26 -0300

    Update Exemplo.py

--- Estruturas-Lineares/Filas/Exemplo.py
+++ Estruturas-Lineares/Filas/Exemplo.py
@@ -1,16 +1,1 @@
-##Listas
 
-# Criando uma lista
-
-frutas = ["maçã", "banana", "laranja"]
-
-# Inserção
-frutas.append("manga")  # Adiciona no final
-frutas.insert(0, "abacaxi")  # Adiciona no início
-
-# Remoção
-frutas.remove("banana")  # Remove o elemento "banana"
-frutas.pop(0)  # Remove o primeiro elemento
-
-# Acesso
-print(frutas[0])         #Saída: laranja
