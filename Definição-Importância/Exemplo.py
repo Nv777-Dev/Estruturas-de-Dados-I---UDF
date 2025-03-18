4e1f0a7296635b6b91f2359d80ff5b530da87db3
Nv777-Dev <nathannaelvictor1206@gmail.com>
2025-03-18 15:08:05 -0300

    Update Exemplo.py

--- Definição-Importância/Exemplo.py
+++ Definição-Importância/Exemplo.py
@@ -1,1 +1,17 @@
- 
+##Exemplo Práticos de Aplicação
+
+#Indexação em Bancos de Dados
+
+class Arvore:
+    def __init__(self, chave):
+        self.chave = chave
+        self.esquerda = None
+        self.direita = None
+
+raiz = Arvore(10)
+raiz.esquerda = Arvore(5)
+raiz.direita = Arvore(15)
+
+print("Raiz:", raiz.chave)                                   #Saída: Raiz: 10
+print("Esquerda da Raiz:", raiz.esquerda.chave)              #Saída: Esquerda da Raiz: 5
+print("Direita da Raiz:", raiz.direita.chave)                #Saída: Direita da Raiz: 15
